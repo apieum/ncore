a7d4b6893393979b259a9565cda0cb4fff0a00b0
Grégory Salvan <apieum@gmail.com>
2015-05-29 15:32:57 +0200

    Update WString with arduino version

--- WString.h
+++ WString.h
@@ -27,7 +27,6 @@
 #include <string.h>
 #include <ctype.h>
 #include <avr/pgmspace.h>
-#include "ultoa.h"
 
 // When compiling programs with this class, the following gcc parameters
 // dramatically increase performance and memory (RAM) efficiency, typically
